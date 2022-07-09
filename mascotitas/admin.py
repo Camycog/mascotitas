a7143366edf216d5fad57cbc5ba9146ca40ada98
Camycog <cami.garcia@duocuc.cl>
2022-07-09 19:26:51 -0400

    muchos cambios

--- mascotitas/admin.py
+++ mascotitas/admin.py
@@ -5,6 +5,9 @@ from .models import Marca, Producto
 class ProductoAdmin(admin.ModelAdmin):
     list_display =  ["nombre", "precio", "nuevo", "marca"]
     list_editable = ["precio"]
+    search_fields = ["nombre"] 
+    list_filter = ["marca", "nuevo"]
+    list_per_page = 5
 
 admin.site.register(Marca)
 admin.site.register(Producto, ProductoAdmin)
