e1069a6118f8c741bec6401ad4d94e9d3ca424b2
Camycog <cami.garcia@duocuc.cl>
2022-07-09 17:27:47 -0400

    Models

--- mascotitas/models.py
+++ mascotitas/models.py
@@ -1,3 +1,21 @@
+from msilib.schema import Class
+from operator import mod
 from django.db import models
 
 # Create your models here.
+class Marca(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50)
+    precio = models.IntegerField()
+    descripcion = models.TextField()
+    nuevo = models.BooleanField()
+    marca = models.ForeignKey(Marca, on_delete=models.PROTECT)
+    fecha_fabricacion = models.DateTimeField()
+
+    def __str__(self):
+        return self.nombre
