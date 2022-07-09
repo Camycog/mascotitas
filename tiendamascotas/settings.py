a3feb5940758ae255c30cdcf64b9bc881f99ca17
Libena <li.jara@duocuc.cl>
2022-07-09 18:00:51 -0400

    Homogeneizados los cambios de línea

--- tiendamascotas/settings.py
+++ tiendamascotas/settings.py
@@ -116,9 +116,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+import os 
+MEDIA_URL = '/media/'
+MEDUA_ROOT = os.path.join(BASE_DIR, "media")
+
 
 
 
