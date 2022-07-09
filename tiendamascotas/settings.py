0ca5fc53f32d06401e6ffaac542eea9c9970122c
Camycog <cami.garcia@duocuc.cl>
2022-07-09 15:57:47 -0400

    cambios

--- tiendamascotas/settings.py
+++ tiendamascotas/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
 
