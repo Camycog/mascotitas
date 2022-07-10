7e742dce9517a4acce9f548a29e5d3641e2350eb
Libena <li.jara@duocuc.cl>
2022-07-10 00:02:27 -0400

    .

--- mascotitas/forms.py
+++ mascotitas/forms.py
@@ -3,7 +3,6 @@ from .models import Contacto
 
 class ContactoForm(forms.ModelForm):
 
-
     class Meta:
         model = Contacto
         #fields = ["nombre", "correo", "tipo_consulta", "mensaje", "avisos"]
