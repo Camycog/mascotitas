c849b06b32a78efd2bfec470e75233e8a22920f2
Camycog <cami.garcia@duocuc.cl>
2022-07-10 10:38:54 -0400

    ¿?

--- mascotitas/urls.py
+++ mascotitas/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path, include
-from .views import home,contacto
+from .views import home,contacto,agregar_producto
 
 
 urlpatterns = [
     path('', home, name="home"),
     path('contacto/', contacto, name="contacto")
+    path('agregar-producto/', agregar_producto, name="agregar_producto")
 ]
