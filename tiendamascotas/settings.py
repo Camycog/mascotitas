c008ee7416bfd522d283ea9cdd27d2c2eaa3e678
Adrian <adr.suarez@duocuc.cl>
2022-07-09 23:18:22 -0400

    aa

--- tiendamascotas/settings.py
+++ tiendamascotas/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 
 # Application definition
 
@@ -41,9 +44,10 @@ INSTALLED_APPS = [
     'mascotitas',
     'colorfield',
     'django.contrib.humanize',
+    'crispy_forms',
     
-
 ]
+CRISPY_TEMPLATE_PACK= 'bootstrap4'
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
