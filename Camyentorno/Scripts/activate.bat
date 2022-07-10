5665944fc4ded29a2c680a32ebdcaa38bf26d072
Camycog <cami.garcia@duocuc.cl>
2022-07-10 12:30:02 -0400

    entorno  otra vez...

--- Camyentorno/Scripts/activate.bat
+++ Camyentorno/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Camila García\OneDrive\Escritorio\mascotitas-4\Camyentorno
+set VIRTUAL_ENV=C:\Users\Camila García\mascotitas-2\Camyentorno
 
 if not defined PROMPT set PROMPT=$P$G
 
