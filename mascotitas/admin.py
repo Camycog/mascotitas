bb380baa0c607dac9da70543800c8da53ed428d9
Camycog <cami.garcia@duocuc.cl>
2022-07-09 22:00:36 -0400

    cambios en adm

--- mascotitas/admin.py
+++ mascotitas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Marca, Producto
+from .models import Marca, Producto, Contacto
 # Register your models here.
 
 class ProductoAdmin(admin.ModelAdmin):
