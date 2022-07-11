d74c7dc1be0b5c5b09c144e8793c09c8be166d47
Camycog <cami.garcia@duocuc.cl>
2022-07-10 21:33:37 -0400

    api lista casi

--- mascotitas/serializer.py
+++ mascotitas/serializer.py
@@ -3,8 +3,10 @@ from .models import Producto
 from rest_framework import serializers
 
 class ProductoSerializer(serializers.ModelSerializer):
+    nombre_marca = serializers.CharField(read_only=True, source="marca.nombre")
     class Meta:
         model = Producto
         fields = '__all__'
+
         
     
