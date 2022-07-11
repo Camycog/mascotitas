d1094d1fcbe247afea7960e6b357180741350004
Libena <li.jara@duocuc.cl>
2022-07-11 17:30:31 -0400

    .

--- carrito/urls.py
+++ carrito/urls.py
@@ -5,8 +5,8 @@ from . import views
 app_name="carrito"
 
 urlpatterns = [
-    path("agregar/<int:producto_id/", views.agregar_carrito, name="agregar"),
-    path("eliminar/<int:producto_id/", views.eliminar_carrito, name="eliminar"),
-    path("restar/<int:producto_id/", views.restar_producto, name="restar"),
-    path("limpiar/", views.limpiar_carrito, name="limpiar"),
+    path("agregar_carrito/<int:producto_id/", views.agregar_carrito, name="agregar"),
+    path("eliminar_carrito/<int:producto_id/", views.eliminar_carrito, name="eliminar"),
+    path("restar_producto/<int:producto_id/", views.restar_producto, name="restar"),
+    path("limpiar_carrito/", views.limpiar_carrito, name="limpiar"),
 ]
