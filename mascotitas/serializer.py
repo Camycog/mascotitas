3b3368d5916c282006d8c9aeaa149e4019b2b7a4
Camycog <cami.garcia@duocuc.cl>
2022-07-10 21:14:59 -0400

    cambios

--- mascotitas/serializer.py
+++ mascotitas/serializer.py
@@ -1,2 +1,9 @@
+from dataclasses import fields
 from .models import Producto
-#from rest_framework import serializers
+from rest_framework import serializers
+
+class ProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Producto
+        fields = '__all__'
+    
