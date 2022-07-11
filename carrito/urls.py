7259a32f088410170c4335edcab6fd326f8bb159
Libena <li.jara@duocuc.cl>
2022-07-11 18:13:26 -0400

    .

--- carrito/urls.py
+++ carrito/urls.py
@@ -2,11 +2,10 @@
 from django.urls import path, include
 from . import views
 
-app_name="carrito"
-
+app_name= "carrito"
 urlpatterns = [
-    path("agregar_carrito/<int:producto_id/", views.agregar_carrito, name="agregar"),
-    path("eliminar_carrito/<int:producto_id/", views.eliminar_carrito, name="eliminar"),
-    path("restar_producto/<int:producto_id/", views.restar_producto, name="restar"),
+    path("agregar_carrito/<int:producto_id>/", views.agregar_carrito, name="agregar"),
+    path("eliminar_carrito/<int:producto_id>/", views.eliminar_carrito, name="eliminar"),
+    path("restar_producto/<int:producto_id>/", views.restar_producto, name="restar"),
     path("limpiar_carrito/", views.limpiar_carrito, name="limpiar"),
 ]
