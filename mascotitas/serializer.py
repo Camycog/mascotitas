749399acb74a812d3b9fd17a9d63dbe51a5124d6
Camycog <cami.garcia@duocuc.cl>
2022-07-10 21:42:05 -0400

    :D

--- mascotitas/serializer.py
+++ mascotitas/serializer.py
@@ -1,9 +1,25 @@
 from dataclasses import fields
-from .models import Producto
+from .models import Marca, Producto
 from rest_framework import serializers
+
+class MarcaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Marca
+        fields = '__all__'
 
 class ProductoSerializer(serializers.ModelSerializer):
     nombre_marca = serializers.CharField(read_only=True, source="marca.nombre")
+    marca = MarcaSerializer(read_only=True)
+    marca_id = serializers.PrimaryKeyRelatedField(queryset=Marca.objects.all(), source="marca")
+    nombre = serializers.CharField(required=True)
+
+    def validate_nombre(self, value):
+        existe = Producto.objects.filter(nombre__iexacr=value).exists()
+
+        if existe:
+            raise serializers.ValidationError("Este producto ya existe")
+
+        return value
     class Meta:
         model = Producto
         fields = '__all__'
